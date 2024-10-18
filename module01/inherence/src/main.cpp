ebda21ca7e563d388da00bb61445468b50c040e9
CanIBeFii <filipe250502@gmail.com>
2024-10-18 11:14:33 +0100

    module 01: inherence: add mini test to functions

--- module01/inherence/src/main.cpp
+++ module01/inherence/src/main.cpp
@@ -2,22 +2,26 @@
 #include "Position.hpp"
 #include "Statistic.hpp"
 #include "Worker.hpp"
+#include "Hammer.hpp"
 
 int main() {
 	{
 		Worker worker(Position(1, 2, 3), Statistic(30, 55));
+		Worker woke;
 		Shovel shovel;
-		std::cout << std::endl;
-
-		worker.useShovel();
-		worker.assignShovel(&shovel);
-		worker.useShovel();
+		Shovel shovel2;
+		Hammer hammer;
 
 		std::cout << std::endl;
-		Worker better_worker;
-		better_worker.assignShovel(&shovel);
-		better_worker.useShovel();
-		worker.useShovel();
+
+		worker.assignTool(&shovel);
+		worker.assignTool(&shovel2);
+		worker.assignTool(&hammer);
+
+		Hammer* hammer_pointer = worker.getTool<Hammer>();
+		if (hammer_pointer != &hammer) {
+			std::cout << "Something went wrong" << std::endl;
+		}
 	}
     return 0;
 }
