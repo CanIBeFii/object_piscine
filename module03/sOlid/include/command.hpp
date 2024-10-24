cbc394f5fad85df57e5cb742c458e6e2d5f54bd7
CanIBeFii <filipe250502@gmail.com>
2024-10-24 20:49:44 +0100

    module03: sOlid: add Command class

--- module03/sOlid/include/command.hpp
+++ module03/sOlid/include/command.hpp
@@ -1,1 +1,32 @@
 #pragma once
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+class Command {
+	int	id;
+	int	date; // TODO implement using correct date class maybe
+	std::string	client;
+	std::vector<std::pair<int, int>>	articles;
+
+	public:
+		Command() {
+			std::cout << "COnstructor for Command called" << std::endl;
+		}
+
+		~Command() {
+			std::cout << "Destructor for Command called" << std::endl;
+		}
+
+		int	get_total_price() {
+			int total = 0;
+			for (std::vector<std::pair<int, int>>::iterator iter = articles.begin();
+				iter != articles.end();
+				++iter)
+			{
+				total += iter->first * iter->second;
+			}
+			return total;
+		}
+};
