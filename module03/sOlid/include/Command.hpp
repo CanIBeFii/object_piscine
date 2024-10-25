ec5d86fbe0c2dc898712375c6268a237975c008f
CanIBeFii <filipe250502@gmail.com>
2024-10-25 13:40:10 +0100

    module03: sOlid: Command now has a functioning constructor and add_article

--- module03/sOlid/include/Command.hpp
+++ module03/sOlid/include/Command.hpp
@@ -5,28 +5,30 @@
 #include <vector>
 
 class Command {
-	int	id;
-	int	date; // TODO implement using correct date class maybe
-	std::string	client;
-	std::vector<std::pair<int, int>>	articles;
+protected:
+  int id;
+  std::string date;
+  std::string client;
+  std::vector<std::pair<int, int>> articles;
 
-	public:
-		Command() {
-			std::cout << "COnstructor for Command called" << std::endl;
-		}
+public:
+  Command(int id_, std::string date_, std::string client_)
+      : id(id_), date(date_), client(client_) {
+    std::cout << "COnstructor for Command called" << std::endl;
+  }
 
-		~Command() {
-			std::cout << "Destructor for Command called" << std::endl;
-		}
+  ~Command() { std::cout << "Destructor for Command called" << std::endl; }
 
-		virtual int	get_total_price() {
-			int total = 0;
-			for (std::vector<std::pair<int, int>>::iterator iter = articles.begin();
-				iter != articles.end();
-				++iter)
-			{
-				total += iter->first * iter->second;
-			}
-			return total;
-		}
+  void add_article(int price, int quantity) {
+    articles.push_back(std::pair<int, int>(price, quantity));
+  }
+
+  virtual int get_total_price() {
+    int total = 0;
+    for (std::vector<std::pair<int, int>>::iterator iter = articles.begin();
+         iter != articles.end(); ++iter) {
+      total += iter->first * iter->second;
+    }
+    return total;
+  }
 };
