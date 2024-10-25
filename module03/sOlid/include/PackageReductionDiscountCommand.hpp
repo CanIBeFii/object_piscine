10a5c255d81ce0e9e123b2a15d8e8f7c0b9c65cd
CanIBeFii <filipe250502@gmail.com>
2024-10-25 13:47:43 +0100

    module03: sOlid: add new Commands with discounts

---
+++ module03/sOlid/include/PackageReductionDiscountCommand.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "Command.hpp"
+#include <iostream>
+
+class PackageReductionDiscountCommand : public Command {
+public:
+  PackageReductionDiscountCommand(int id_, std::string date_,
+                                  std::string client_)
+      : Command(id_, date_, client_) {
+    std::cout << "Constructor for PackageReductionDiscountCommand called"
+              << std::endl;
+  }
+
+  ~PackageReductionDiscountCommand() {
+    std::cout << "Destructor for PackageReductionDiscountCommand called"
+              << std::endl;
+  }
+
+  int get_total_price() {
+    int total = 0;
+    for (std::vector<std::pair<int, int>>::iterator iter = articles.begin();
+         iter != articles.end(); ++iter) {
+      total += iter->first * iter->second;
+    }
+    if (total > 150) {
+      return total * 0.9;
+    }
+    return total;
+  }
+};
---
+++ module03/sOlid/include/TuesdayDiscountCommand.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "Command.hpp"
+
+class TuesdayDiscountCommand : public Command {
+public:
+  TuesdayDiscountCommand(int id_, std::string date_, std::string client_)
+      : Command(id_, date_, client_) {
+    std::cout << "Constructor for TuesdayDiscountCommand called" << std::endl;
+  }
+
+  ~TuesdayDiscountCommand() {
+    std::cout << "Destructor for TuesdayDiscountCommand called" << std::endl;
+  }
+
+  int get_total_price() {
+    int total = 0;
+    for (std::vector<std::pair<int, int>>::iterator iter = articles.begin();
+         iter != articles.end(); ++iter) {
+      total += iter->first * iter->second;
+    }
+    if (date == "Tuesday") {
+      return total * 0.9;
+    }
+    return total;
+  }
+};
