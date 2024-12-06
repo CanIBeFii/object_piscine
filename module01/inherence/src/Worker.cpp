d1db19f996c0c747b5847ae5080506dacb0b811c
CanIBeFii <filipe250502@gmail.com>
2024-12-06 11:10:28 +0000

    module01:inherence: finish Worker stuff

--- module01/inherence/src/Worker.cpp
+++ module01/inherence/src/Worker.cpp
@@ -1,4 +1,7 @@
 #include "Worker.hpp"
+#include "Tool.hpp"
+
+std::map<Tool *, Worker *> Worker::who_has_tool;
 
 Worker::Worker() : coordonnee(Position()), stat(Statistic()) {
   std::cout << "Worker default constructor called" << std::endl;
@@ -36,21 +39,34 @@ Statistic &Worker::getStat() {
 
 void Worker::assignTool(Tool *new_tool) {
   std::cout << "Assign tool called" << std::endl;
-  if (new_tool) {
-    new_tool->leaveWorker();
-    tools.insert(new_tool);
-    new_tool->assignWorker(this);
+  if (new_tool == NULL) {
+    return;
   }
+  for (std::map<Tool *, Worker *>::iterator iter = who_has_tool.begin();
+       iter != who_has_tool.end(); ++iter) {
+    if (iter->first == new_tool) {
+      if (iter->second != NULL) {
+        iter->second->removeTool(new_tool);
+      }
+      iter->second = this;
+      return;
+    }
+  }
+  who_has_tool.insert(std::pair<Tool *, Worker *>(new_tool, this));
 }
 
 void Worker::removeTool(Tool *tool) {
   std::cout << "Remove shovel called" << std::endl;
-  if (tool) {
+  if (tool == NULL) {
+    return;
+  }
+  std::set<Tool *>::iterator tool_iter = tools.find(tool);
+  if (tool_iter != tools.end()) {
     tools.erase(tool);
-    std::map<Tool *, Worker *>::iterator iter = who_has_tool.find(tool);
-    if (iter != who_has_tool.end()) {
-      iter->second = NULL;
-    }
+  }
+  std::map<Tool *, Worker *>::iterator iter = who_has_tool.find(tool);
+  if (iter != who_has_tool.end()) {
+    iter->second = NULL;
   }
 }
 
