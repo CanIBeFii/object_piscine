16840c5e17151ca313a172dc594e1d2377979e76
CanIBeFii <filipe250502@gmail.com>
2025-02-10 13:38:40 +0000

    feat: finish implementing singleton

--- module04/ex01/include/SingletonList.hpp
+++ module04/ex01/include/SingletonList.hpp
@@ -1,11 +1,12 @@
 #pragma once
 
+#include <stdexcept>
 #include <vector>
 
 template <typename T> class SingletonList {
 private:
-  std::vector<T> m_list;
-  static SingletonList *instance_ptr = nullptr;
+  std::vector<T *> _list;
+  static SingletonList *_instance_ptr = nullptr;
 
   SingletonList() {}
   ~SingletonList() {}
@@ -14,8 +15,28 @@ public:
   SingletonList(const SingletonList &) = delete;
   SingletonList &operator=(const SingletonList) = delete;
 
-  SingletonList *getInstance() {
-    if (instance_ptr == nullptr) {
+  static SingletonList *getInstance() {
+    if (_instance_ptr == nullptr) {
+      _instance_ptr = new SingletonList();
     }
+    return _instance_ptr;
+  }
+
+  void add_element(T *element) { _list.push_back(element); }
+  bool delete_element(T *element) {
+    for (typename std::vector<T *>::iterator iter = _list.begin();
+         iter != _list.end(); ++iter) {
+      if (element == *iter) {
+        _list.erase(iter);
+        return true;
+      }
+    }
+    return false;
+  }
+  std::vector<T *> get_list() { return _list; }
+  T *operator[](int index) {
+    if (index < 0 || index >= _list.size())
+      throw std::out_of_range("Index is not in range of the list");
+    return _list.at(index);
   }
 };
